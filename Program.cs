e5139c996a004a1d638a2e1a45ff489287f922ce
IT <IT@PCE-04>
2025-11-21 08:43:32 -0400

    create structure

--- Program.cs
+++ Program.cs
@@ -1,25 +1,64 @@
+using Marketing_LP.Core.Interfaces;
+using Marketing_LP.Core.Services;
+using Marketing_LP.Infrastructure.Data;
+using Marketing_LP.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Database Configuration
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Dependency Injection - Core Services
+builder.Services.AddScoped<ILeadService, LeadService>();
+builder.Services.AddScoped<ICampaignService, CampaignService>();
+
+// Dependency Injection - Repositories
+builder.Services.AddScoped<ILeadRepository, LeadRepository>();
+builder.Services.AddScoped<ICampaignRepository, CampaignRepository>();
+
+// CORS Configuration - PARA TODOS LOS MÓDULOS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllModules", policy =>
+    {
+        policy.AllowAnyOrigin()  // Para desarrollo - en producción especifica los dominios
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Usar CORS - IMPORTANTE: Debe ir antes de UseAuthorization
+app.UseCors("AllowAllModules");
 
 app.UseAuthorization();
-
 app.MapControllers();
+
+// Initialize Database
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    await context.Database.EnsureCreatedAsync();
+
+    // Seed inicial de datos
+    await SeedData.InitializeAsync(context);
+}
 
 app.Run();
--- obj/Debug/net8.0/Marketing_LP.AssemblyInfo.cs
+++ obj/Debug/net8.0/Marketing_LP.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Marketing_LP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2d29702486d31784e84b215966ad60d642d8516")]
 [assembly: System.Reflection.AssemblyProductAttribute("Marketing_LP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Marketing_LP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
